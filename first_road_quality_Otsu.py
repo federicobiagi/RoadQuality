72a1a6d3400248512f01843dfdc5ea7356c1819f
federicobiagi <federico.biagi98@gmail.com>
2022-06-29 02:27:11 +0200

    commit on master

--- first_road_quality_Otsu.py
+++ first_road_quality_Otsu.py
@@ -5,6 +5,7 @@ from cv2 import imshow
 from cv2 import THRESH_BINARY
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 print("OpenCV version is: " + cv2.__version__)
 
@@ -15,6 +16,7 @@ cap = cv2.VideoCapture("D:\Road_Videos\\Road2.mp4")
 
 if not cap.isOpened():
     print("Could not open video file")
+
 
 cv2.namedWindow("original", cv2.WINDOW_NORMAL)
 cv2.namedWindow("edge", cv2.WINDOW_NORMAL)
@@ -27,13 +29,14 @@ plt.ion()  # Turns interactive mode on (probably unnecessary)
 
 while cap.isOpened():
     valid, frame = cap.read()
-    
-    
+    #print(frame.dtype)
+    #frame = np.clip(frame,0,255)
     height, width, _ = frame.shape
     #frame = frame[round(height / 1.4):height, round(width / 4):round(width / 4) * 3, :]
     fps = cap.get(5)
+    #to produce the gray frame we sum along the channels thus eliminating the third dimension, the result is a gray scale image with infact no channel
     gray_frame = frame[:, :, 0] + frame[:, :, 1] + frame[:, :, 2]
-
+    
     #normalization
     #frame = frame.astype("float32")
     #grayframe = frame[:,:,0] + frame[:,:,1] + frame[:,:,2]
@@ -46,10 +49,8 @@ while cap.isOpened():
     #gray_frame = cv2.GaussianBlur(gray_frame,(5,5),0)
 
 
-    #viewer.clear()
-    #viewer.hist(gray_frame.flatten(), bins=255)
-    #fig.canvas.draw()
-    #plt.pause(0.08)
+    viewer.clear()
+    
 
     if valid:
         #the first parameter needs to be a gray scale image, the second parameter determines the threshold value,
@@ -57,6 +58,8 @@ while cap.isOpened():
         #thresholding types liek otsu. The method determines the best threshold to use between the two specified thresholds.
         th,edge = cv2.threshold(gray_frame, thresh= 0, maxval=255,type = cv2.THRESH_OTSU)
        
+        
+
         cv2.imshow('original', frame)
         cv2.imshow('edge', edge)
         cv2.waitKey(5)
